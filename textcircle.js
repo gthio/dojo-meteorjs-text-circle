1a9cc078cb7b4a3202be92ca04c05837d6e1fee0
gthio <gunawanthio@outlook.com>
2016-04-25 05:45:42 +0800

    HTML preview

--- textcircle.js
+++ textcircle.js
@@ -14,9 +14,16 @@ if (Meteor.isClient){
       else{
         return undefined;
       }
+    },
+    config: function() {
+      return function(editor) {
+        editor.on("change", function(cm_editor, info) {
+          $("#viewer_iframe").contents().find("html")
+            .html(cm_editor.getValue());
+        })
+      }
     }
   });
-  
 }
 
 if (Meteor.isServer){
