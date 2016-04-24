273fbf6f153260910765cc0920c99d810e5e23d0
gthio <gunawanthio@outlook.com>
2016-04-25 05:13:49 +0800

    Bootstrap it - clean up codes

--- textcircle.js
+++ textcircle.js
@@ -2,16 +2,6 @@ this.Documents = new Mongo.Collection("documents");
 
 if (Meteor.isClient){
   
-  Meteor.setInterval(function(){
-    Session.set("current_date", new Date());
-  }, 1000)
-  
-  Template.date_display.helpers({
-    current_date: function(){
-      return Session.get("current_date");
-    }
-  });
-  
   Template.editor.helpers({
     docid: function(){
       console.log("doc id helper:");
