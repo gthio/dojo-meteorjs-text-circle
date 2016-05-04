8959af53903aa94a5bb30a5cf167457cb00c4e86
gthio <gunawanthio@outlook.com>
2016-05-04 21:23:54 +0800

    Who is editing - DB

--- textcircle.js
+++ textcircle.js
@@ -41,6 +41,33 @@ if (Meteor.isServer){
 
 Meteor.methods({
   addEditingUser: function(){
-    EditingUsers.insert({user: "Test123"});
+    
+    var doc, user, eusers;
+    
+    doc = Documents.findOne();
+    
+    if (!doc){
+      return;
+    }
+    
+    if (!this.userId){
+      return;
+    }
+    
+    user = Meteor.user().profile;
+    
+    eusers = EditingUsers.findOne({docid: doc._id});
+    
+    if (!eusers){
+      eusers = {
+        docid: doc._id,
+        users: {}
+      };
+    }
+    
+    user.lastEdit = new Date();
+    eusers.users[this.userId] = user;
+    
+    EditingUsers.upsert({_id: eusers._id}, eusers);    
   }
 })
