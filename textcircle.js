f61cca855e434dbf459dd99e80adb398c4d69e4e
gthio <gunawanthio@outlook.com>
2016-05-05 05:36:33 +0800

    Who is editing - UI

--- textcircle.js
+++ textcircle.js
@@ -29,6 +29,33 @@ if (Meteor.isClient){
       }
     }
   });
+  
+  Template.editingUsers.helpers({
+    users: function() {
+      var doc, eusers,users;
+      
+      doc = Documents.findOne();
+      
+      if (!doc){
+        return;
+      }
+      
+      eusers = EditingUsers.findOne({ docid: doc._id});
+      
+      if (!eusers){
+        return;
+      }
+      
+      users = new Array();
+      var i = 0;
+      for (var user_id in eusers.users){
+        users[i] = fixObjectKeys(eusers.users[user_id]);
+        i++;
+      }
+      
+      return users;
+    }
+  })
 }
 
 if (Meteor.isServer){
@@ -71,3 +98,16 @@ Meteor.methods({
     EditingUsers.upsert({_id: eusers._id}, eusers);    
   }
 })
+
+//Rename object by removing hyphens for spacebar templating
+function fixObjectKeys(obj){
+  var newObj = {};
+    
+  for (key in obj){
+    var key2 = key.replace("-", "");
+      
+    newObj[key2] = obj[key];
+  }
+    
+  return newObj;
+}
