ea68085dcb1aa19277985dc027116e422918b3d4
gthio <gunawanthio@outlook.com>
2016-05-06 05:50:28 +0800

    Docs in the DB - add new docs to db

--- textcircle.js
+++ textcircle.js
@@ -62,6 +62,13 @@ if (Meteor.isClient){
     "click .js-add-doc": function(event){
       event.preventDefault();
       console.log("Add new doc");
+      
+      if (!Meteor.user()){
+        alert("You need to login first!");
+      }
+      else{
+        Meteor.call("addDoc");
+      }
     }
   })
 }
@@ -75,6 +82,21 @@ if (Meteor.isServer){
 }
 
 Meteor.methods({
+  addDoc: function(){
+    var doc;
+
+    if (!this.userId){
+      return;
+    }
+    else{
+      doc = {owner: this.userId, 
+        createdOn: new Date(),
+        title:"my new doc"};
+
+      Documents.insert(doc);
+    }
+  },
+  
   addEditingUser: function(){
     
     var doc, user, eusers;
